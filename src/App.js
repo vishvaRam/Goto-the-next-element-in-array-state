449164a3ba5db09669900a217e27dca29abbec0a
vishvaRam <44122284+vishvaRam@users.noreply.github.com>
2019-09-30 11:54:10 +0100

    working looping through array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import data from "./data/data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var index = 0;
+export default class App extends Component {
+  state = {
+    items: data.items,
+    item: data.items[index]
+  };
+
+  handleOnClickNext = () => {
+    if (this.state.items.length-1 === index) {
+      return ;
+    }
+    else{
+      index++;
+      this.setState(prevState => ({
+        item: prevState.items[index]
+      }));
+      console.log(index);
+    }
+  };
+
+  handleOnClickPrev =()=>{
+    if (0 === index) {
+      return ;
+    }
+    else{
+      index--;
+      this.setState(prevState => ({
+        item: prevState.items[index]
+      }));
+    }
+  }
+
+  render() {
+    const { id, item } = this.state.item;
+    return (
+      <div>
+        <div>
+          <div>{id}</div>
+          <div>{item}</div>
+          <button onClick={this.handleOnClickNext}>Next</button>
+          <button onClick={this.handleOnClickPrev}>prev</button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+const data={
+    "items": [
+        { id: 1, item: "milk" },
+        { id: 2, item: "eggs" },
+        { id: 3, item: "choco" },
+        { id: 4, item: "suger" }
+      ]
+}
+
+export default data;
